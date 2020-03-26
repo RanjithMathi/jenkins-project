11f87cf8ae654ae1c3f6a829357ab772cc46c786
unknown <mranjith2016@gmail.com>
2020-03-26 13:17:55 +0530

    jenkins cd and ci

---
+++ src/main/java/com/tvscs/TvsApplication.java
@@ -0,0 +1,29 @@
+package com.tvscs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TvsApplication extends SpringBootServletInitializer {
+  
+	public static void main(String[] args) {
+		SpringApplication.run(TvsApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TvsApplication.class);
+	}
+
+	@GetMapping(value = "/getJenkins")
+	public String getjenkins() {
+     
+		return "jenkins value";
+	}
+}
+  
